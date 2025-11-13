0614dd400c07339e8bac21cf6d6fb8cc33e279ba
Egy Alfarezi <egyafz.049@gmail.com>
2025-11-13 23:20:44 +0700

    Update TextField to login page

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:presensi_kita/views/widget_tree.dart';
 import 'package:presensi_kita/views/widgets/hero_widget.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,12 +10,14 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController controller = TextEditingController();
+  TextEditingController controllerUN = TextEditingController();
+  TextEditingController controllerPW = TextEditingController();
 
   @override
   void dispose() {
     // TODO: implement dispose
-    controller.dispose();
+    controllerUN.dispose();
+    controllerPW.dispose();
     super.dispose();
   }
 
@@ -29,11 +32,47 @@ class _LoginPageState extends State<LoginPage> {
             HeroWidget(title: 'Login'),
             SizedBox(height: 20.0),
             TextField(
-              controller: controller,
-              decoration: InputDecoration(border: OutlineInputBorder()),
+              controller: controllerUN,
+              decoration: InputDecoration(
+                hintText: 'Username',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(14.0),
+                ),
+              ),
               onEditingComplete: () {
                 setState(() {});
               },
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              controller: controllerPW,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(14.0),
+                ),
+              ),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            SizedBox(height: 20.0),
+            FilledButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WidgetTree();
+                    },
+                  ),
+                );
+              },
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.green[800],
+                minimumSize: Size(double.infinity, 32.0),
+              ),
+              child: Text('Login'),
             ),
           ],
         ),
