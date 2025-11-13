e7c717fc1a1593316349ed1061e5fc04d099d3f3
Egy Alfarezi <egyafz.049@gmail.com>
2025-11-13 22:29:32 +0700

    Add snackbar and alertdialog

--- lib/views/pages/settings_page.dart
+++ lib/views/pages/settings_page.dart
@@ -24,9 +24,41 @@ class _SettingsPageState extends State<SettingsPage> {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextField(
-              decoration: InputDecoration(border: OutlineInputBorder()),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Test SnackBar'),
+                    behavior: SnackBarBehavior.floating,
+                    duration: Duration(seconds: 1),
+                  ),
+                );
+              },
+              child: Text('Click me'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Alert Title'),
+                      content: Text('Lorem ipsum'),
+                      actions: [
+                        FilledButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Close'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text('Open dialog'),
             ),
           ],
         ),
