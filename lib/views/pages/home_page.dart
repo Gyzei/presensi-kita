57e309e20499ff74eb471d7a25688e7a6e0d984e
Egy Alfarezi <egyafz.049@gmail.com>
2025-11-18 12:03:05 +0700

    Add container_widget

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:presensi_kita/data/constants.dart';
+import 'package:presensi_kita/views/widgets/container_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,26 +7,46 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(20.0),
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: SingleChildScrollView(
         child: Column(
           children: [
             // HeroWidget(title: 'Login'),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              child: Card(
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Title', style: KTextStyle.titleTealText),
-                      Text('Lorem ipsum.', style: KTextStyle.descriptionText),
-                    ],
-                  ),
-                ),
-              ),
+            ContainerWidget(
+              title: 'Basic Layout',
+              desc: 'Lorem ipsum dolor sit amet',
+            ),
+            ContainerWidget(
+              title: 'Basic Layout',
+              desc: 'Lorem ipsum dolor sit amet',
+            ),
+            ContainerWidget(
+              title: 'Basic Layout',
+              desc: 'Lorem ipsum dolor sit amet',
+            ),
+            ContainerWidget(
+              title: 'Basic Layout',
+              desc: 'Lorem ipsum dolor sit amet',
+            ),
+            ContainerWidget(
+              title: 'Basic Layout',
+              desc: 'Lorem ipsum dolor sit amet',
+            ),
+            ContainerWidget(
+              title: 'Basic Layout',
+              desc: 'Lorem ipsum dolor sit amet',
+            ),
+            ContainerWidget(
+              title: 'Basic Layout',
+              desc: 'Lorem ipsum dolor sit amet',
+            ),
+            ContainerWidget(
+              title: 'Basic Layout',
+              desc: 'Lorem ipsum dolor sit amet',
+            ),
+            ContainerWidget(
+              title: 'Basic Layout',
+              desc: 'Lorem ipsum dolor sit amet',
             ),
           ],
         ),
---
+++ lib/views/widgets/container_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:presensi_kita/data/constants.dart';
+
+class ContainerWidget extends StatelessWidget {
+  const ContainerWidget({super.key, required this.title, required this.desc});
+
+  final String title;
+  final String desc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(top: 10.0),
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title, style: KTextStyle.titleTealText),
+              Text(desc, style: KTextStyle.descriptionText),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
