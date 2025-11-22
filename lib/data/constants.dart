27f77f2867d9434ae8adbbb0038ce14deb95e072
Egy Alfarezi <egyafz.049@gmail.com>
2025-11-22 18:43:36 +0700

    Add some TextStyle Constants

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class KColors {
   static Color accentColor = Color.fromRGBO(213, 160, 7, 1);
+  static Color shadowColor = Color.fromRGBO(32, 32, 32, 0.3);
 
   static Color lightBackgroundColor = Color.fromRGBO(248, 244, 232, 1);
   static Color darkBackgroundColor = Color.fromRGBO(51, 51, 51, 1);
@@ -15,6 +16,104 @@ class KConstants {
 }
 
 class KTextStyle {
+  // FontSize 14
+  static const TextStyle font12w500 = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle font12w600 = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle font12w700 = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w700,
+  );
+
+  // FontSize 14
+  static const TextStyle font14w500 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle font14w600 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle font14w700 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w700,
+  );
+
+  // FontSize 16
+  static const TextStyle font16w500 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle font16w600 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle font16w700 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w700,
+  );
+
+  // FontSize 18
+  static const TextStyle font18w500 = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle font18w600 = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle font18w700 = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w700,
+  );
+
+  // FontSize 20
+  static const TextStyle font20w500 = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle font20w600 = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle font20w700 = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w700,
+  );
+
+  // FontSize 24
+  static const TextStyle font24w500 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle font24w600 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle font24w700 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w700,
+  );
+
+  // FontSize 28
+  static const TextStyle font28w500 = TextStyle(
+    fontSize: 28.0,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle font28w600 = TextStyle(
+    fontSize: 28.0,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle font28w700 = TextStyle(
+    fontSize: 28.0,
+    fontWeight: FontWeight.w700,
+  );
+
   static const TextStyle appBarTitle = TextStyle(
     fontSize: 20.0,
     fontWeight: FontWeight.w500,
